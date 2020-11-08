0139e10f64a31396853a0213c90e15b4d79a163b
Iago <iagobrazmendes@gmail.com>
2020-11-07 21:11:22 -0300

    add menu tab

---
+++ src/components/Menu.tsx
@@ -0,0 +1,49 @@
+import Link from 'next/link'
+
+import { FiUser } from 'react-icons/fi'
+
+import styles from '../styles/components/menu.module.css'
+import logo from '../assets/logo.svg'
+
+export default function MenuTabs()
+{
+    return (
+        <div id={styles.tabsContainer}>
+            <Link href="/">
+                <img src={logo} alt="Cruz Representações" />
+            </Link>
+            <ul>
+                <Link href="/companies">
+                    <a className={styles.link}>
+                        Empresas
+                    </a>
+                </Link>
+                <Link href="/">
+                    <a className={styles.link}>
+                        Catálogo
+                    </a>
+                </Link>
+                <Link href="/contact">
+                    <a className={styles.link}>
+                        Contato
+                    </a>
+                </Link>
+                <Link href="/about">
+                    <a className={styles.link}>
+                        Sobre
+                    </a>
+                </Link>
+                <Link href="/">
+                    <a className={styles.linkBlock}>
+                        Fazer pedido
+                    </a>
+                </Link>
+                <Link href="/login">
+                    <a className={styles.linkUser}>
+                        <FiUser size={30} color="#5e5d5d" />
+                    </a>
+                </Link>
+            </ul>
+        </div>
+    )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,16 @@
 import {AppProps} from 'next/app'
 
+import Menu from '../components/Menu'
 import '../styles/global.css'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) =>
 {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Menu />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
