c360649f06db542f82189cc2e469293060b72302
Iago <iagobrazmendes@gmail.com>
2020-11-09 15:02:19 -0300

    add home page content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,22 @@
 import Head from 'next/head'
 
+import illustration from '../assets/illustration1.svg'
+import Carousel from '../components/Carousel'
+
 export default function Home() {
   return (
-    <div className="container">
+    <div id="home">
       <Head>
         <title>Cruz representações</title>
       </Head>
-
-      <h1>Hello world!</h1>
+      <Carousel />
+      <main>
+          <img src={illustration} alt="Homem mexendo no computador"/>
+          <div className="name">
+              <h1>Cruz Representações</h1>
+              <h2>Excelência em representação comercial no Norte de Minas Gerais.</h2>
+          </div>
+      </main>
     </div>
   )
 }
