2df71be3493d683390a03dd1b5986e6c79fbf9dc
Iago <iagobrazmendes@gmail.com>
2020-11-19 20:10:40 -0300

    create companies page

--- src/pages/empresas.tsx
+++ src/pages/empresas.tsx
@@ -1,10 +1,23 @@
+import { GetStaticProps } from 'next'
 import {useSession} from 'next-auth/client'
-import Loading from '../components/Loading'
-import NotLogged from '../components/NotLogged'
 
 import Container from '../styles/pages/empresas'
+import Loading from '../components/Loading'
+import NotLogged from '../components/NotLogged'
+import api from '../services/api'
 
-const Companies = () =>
+interface CompaniesProps
+{
+	companies: Array<
+	{
+		id: string
+		imagem: string
+		nome_fantasia: string
+		descricao_curta: string
+	}>
+}
+
+const Companies: React.FC<CompaniesProps> = ({companies}) =>
 {
 	const [session, loading] = useSession()
 
@@ -12,10 +25,33 @@ const Companies = () =>
 	if (!session) return <NotLogged />
 
 	return (
-		<Container>
-			<h1>Empresas</h1>
+		<Container className='container'>
+			{companies.map(company => (
+				<div className='flipCard' key={company.id}>
+						<div className='companyCard'>
+						<div className='front'>
+							<img src={company.imagem} alt={company.nome_fantasia} />
+						</div>
+						<div className='back'>
+							<h1>{company.nome_fantasia}</h1>
+							<h2>{company.descricao_curta}</h2>
+						</div>
+					</div>
+				</div>
+			))}
 		</Container>
 	)
+}
+
+export const getStaticProps: GetStaticProps = async ctx =>
+{
+	let companies = []
+	await api.get('companies').then(res => companies = res.data)
+
+	return {
+		props: {companies},
+		revalidate: 10
+	}
 }
 
 export default Companies
--- src/styles/pages/empresas.ts
+++ src/styles/pages/empresas.ts
@@ -1,5 +1,98 @@
 import styled from 'styled-components'
 
-const Container = styled.div``
+const Container = styled.div`
+	display: grid;
+	grid-auto-rows: 25rem;
+	grid-template-columns: repeat(auto-fill, 25rem);
+	grid-template-columns: repeat(auto-fill, minmax(25rem, 1fr));
+	grid-gap: 2rem;
+	align-items: center;
+	justify-items: center;
+
+	padding: 5rem;
+	padding-left: 10rem;
+	padding-right: 10rem;
+
+	.flipCard
+	{
+		width: 25rem;
+		height: 25rem;
+		perspective: 500px;
+		
+		.companyCard
+		{
+			position: relative;
+			transform-style: preserve-3d;
+			transition: 0.5s;
+			cursor: pointer;
+
+			width: 100%;
+			height: 100%;
+
+			.front, .back
+			{
+				backface-visibility: hidden;
+				position: absolute;
+
+				width: 100%;
+				height: 100%;
+
+				display: flex;
+				align-items: center;
+				justify-content: center;
+			}
+
+			.front
+			{
+				border: ${p => p.theme.colors.primary} solid 1px;
+				border-radius: 1rem;
+				background-color: #fff;
+
+				img
+				{
+					width: 100%;
+					height: auto;
+				}
+			}
+
+			.back
+			{
+				transform: rotateY(180deg);
+				transition: 0.5s;
+
+				flex-direction: column;
+				justify-content: space-around;
+				padding-top: 5rem;
+				border-radius: 1rem;
+
+				color: ${p => p.theme.colors.secondary};
+
+				h1
+				{
+					font-family: Ubuntu;
+					font-weight: 700;
+					font-size: 3.5rem;
+				}
+
+				h2
+				{
+					font-family: Roboto;
+					font-weight: 400;
+					font-size: 2rem;
+				}
+			}
+		}
+
+		:hover .companyCard
+		{
+			transform: rotateY(180deg);
+
+			.back
+			{
+				background-color: ${p => p.theme.colors.primary};
+			}
+		}
+	}
+`
 
 export default Container
