347c8544b5584163424478f2099c57ab023b12c3
Iago <iagobrazmendes@gmail.com>
2020-11-21 18:03:44 -0300

    change menu style to styled-components

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,33 +1,34 @@
 import Link from 'next/link'
 import {useRouter} from 'next/router'
-
-import { FiUser, FiMenu, FiX } from 'react-icons/fi'
+import {FiUser, FiMenu, FiX} from 'react-icons/fi'
 
 import logo from '../assets/logo.svg'
-import { useEffect, useState } from 'react'
+import {useEffect, useState} from 'react'
+import Container from '../styles/components/Menu'
 
 export default function MenuTabs()
 {
-		const Router = useRouter()
+	const Router = useRouter()
 
-		const [width, setWidth] = useState(1500)
-		const [isBurgerOpen, setIsBurgerOpen] = useState(false)
-		
-		useEffect(() =>
-		{
-			setWidth(window.innerWidth)
-			window.addEventListener("resize", () => setWidth(window.innerWidth))
-		}, [])
+	const [width, setWidth] = useState(1500)
+	const [isBurgerOpen, setIsBurgerOpen] = useState(false)
+	
+	useEffect(() =>
+	{
+		setWidth(window.innerWidth)
+		window.addEventListener("resize", () => setWidth(window.innerWidth))
+	}, [])
 
-		if (Router.pathname === '/login') return null
-		
-		if (width < 1000) return (
-			<div id="menu">
-				<Link href="/">
-					<img src={logo} alt="Cruz Representações" />
-				</Link>
-				{
-					isBurgerOpen
+	if (Router.pathname === '/login') return null
+
+	return (
+		<Container>
+			<Link href="/">
+				<img src={logo} alt="Cruz Representações" />
+			</Link>
+			{
+				width < 1000
+				? isBurgerOpen
 					? (
 						<button onClick={() => setIsBurgerOpen(false)}>
 							<FiX size={30} />
@@ -38,50 +39,41 @@ export default function MenuTabs()
 							<FiMenu size={30} />
 						</button>
 					)
-				}
-				{/* <div id="burger">
-					<h1>Bla</h1>
-				</div> */}
-			</div>
-		)
-
-    return (
-        <div id="menu">
-            <Link href="/">
-                <img src={logo} alt="Cruz Representações" />
-            </Link>
-            <ul>
-                <Link href="/empresas">
-                    <a className="link">
-                        Empresas
-                    </a>
-                </Link>
-                <Link href="/">
-                    <a className="link">
-                        Catálogo
-                    </a>
-                </Link>
-                <Link href="/contato">
-                    <a className="link">
-                        Contato
-                    </a>
-                </Link>
-                <Link href="/">
-                    <a className="link">
-                        Sobre
-                    </a>
-                </Link>
-                <Link href="/">
-                    <a className="linkBlock">
-                        Fazer pedido
-                    </a>
-                </Link>
-                <Link href="/login">
-                    <a className="linkUser">
-                        <FiUser size={30} color="#5e5d5d" />
-                    </a>
-                </Link>
-            </ul>
-        </div>
-    )
+				: (
+					<ul>
+						<Link href="/empresas">
+								<a className="link">
+										Empresas
+								</a>
+						</Link>
+						<Link href="/">
+								<a className="link">
+										Catálogo
+								</a>
+						</Link>
+						<Link href="/contato">
+								<a className="link">
+										Contato
+								</a>
+						</Link>
+						<Link href="/">
+								<a className="link">
+										Sobre
+								</a>
+						</Link>
+						<Link href="/">
+								<a className="linkBlock">
+										Fazer pedido
+								</a>
+						</Link>
+						<Link href="/login">
+								<a className="linkUser">
+										<FiUser size={30} color="#5e5d5d" />
+								</a>
+						</Link>
+					</ul>
+				)
+			}
+		</Container>
+	)
 }
---
+++ src/styles/components/Menu.ts
@@ -0,0 +1,97 @@
+import styled from 'styled-components'
+
+const Container = styled.nav`
+	height: 7.5rem;
+	width: 100%;
+
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+
+	background-color: ${p => p.theme.colors.primary};
+
+	img
+	{
+		width: 10rem;
+
+		margin-left: 2.5rem;
+
+		cursor: pointer;
+		transition: 0.25s;
+
+		:hover
+		{
+			transform: scale(1.1);
+		}
+	}
+
+	button
+	{
+		padding: 1rem;
+		background: none;
+		border: none;
+
+		color: ${p => p.theme.colors.secondary};
+		margin-right: 2.5rem;
+	}
+
+	ul
+	{
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		width: 50%;
+
+		margin-right: 2rem;
+
+		font-family: Ubuntu;
+
+		.link
+		{
+			color: ${p => p.theme.colors.secondary};
+			text-decoration: none;
+			font-size: 1.75rem;
+			transition: 0.25s;
+
+			:hover
+			{
+				text-shadow: 0px 0px 10px ${p => p.theme.colors.secondary};
+			}
+		}
+
+		.linkBlock
+		{
+			color: ${p => p.theme.colors.secondary};
+			text-decoration: none;
+			font-size: 1.75rem;
+			transition: 0.25s;
+
+			border-width: 1px;
+			border-style: solid;
+			border-color: ${p => p.theme.colors.secondary};
+			padding: 1rem;
+			border-radius: 100px;
+
+			:hover
+			{
+				background-color: ${p => p.theme.colors.secondary};
+				color: ${p => p.theme.colors.primary};
+			}
+		}
+
+		.linkUser
+		{
+			background-color: #C4C4C4;
+			border-radius: 100px;
+			padding: 1rem;
+			transition: 0.25s;
+
+			:hover
+			{
+				transform: scale(1.1);
+			}
+		}
+	}
+`
+
+export default Container
