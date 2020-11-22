81b2a7914da53f46921d0002425ac413a81dd8d8
Iago <iagobrazmendes@gmail.com>
2020-11-22 11:40:59 -0300

    create catalog page

---
+++ src/pages/catalogo/[company]/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Container from '../../../styles/pages/catalogo/[company]/index'
+
+const CompanyLines: React.FC = () =>
+{
+	return (
+		<Container>
+			<Head>
+				<title>Alklin — Linhas | Cruz Representações</title>
+			</Head>
+		</Container>
+	)
+}
+
+export default CompanyLines
