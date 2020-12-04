3d02988d51f7d78efaa5156e96fff7c845a2c3ee
Iago <iagobrazmendes@gmail.com>
2020-12-04 15:56:35 -0300

    make about page responsive

--- src/pages/sobre.tsx
+++ src/pages/sobre.tsx
@@ -25,45 +25,48 @@ const About: React.FC<AboutProps> = ({sellers}) =>
 				<title>Sobre | Cruz Representações</title>
 			</Head>
 
-			<div className='cardGroup history'>
-				<span>História</span>
-				<img src={logo} alt='Cruz representações' />
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam massa massa, cursus eget ultrices sed, laoreet non sapien. Mauris vitae pellentesque metus. Suspendisse scelerisque molestie felis, sit amet iaculis turpis. Quisque sagittis vehicula enim, vitae hendrerit magna ullamcorper vel. Quisque eu imperdiet tellus, sit amet porta tellus. Vestibulum vehicula facilisis justo, eu feugiat tortor placerat tincidunt. Aenean vitae est vitae neque venenatis semper. Mauris velit metus, volutpat et fringilla a, auctor eu massa. Suspendisse nec neque eget risus dapibus maximus et id metus. Aliquam vitae eleifend tellus. Etiam vel nisl vitae nisi pulvinar dignissim non in eros. </p>
-			</div>
+			<main>
+				<div className='cardGroup history'>
+					<span>História</span>
+					<img src={logo} alt='Cruz representações' />
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam massa massa, cursus eget ultrices sed, laoreet non sapien. Mauris vitae pellentesque metus. Suspendisse scelerisque molestie felis, sit amet iaculis turpis. Quisque sagittis vehicula enim, vitae hendrerit magna ullamcorper vel. Quisque eu imperdiet tellus, sit amet porta tellus. Vestibulum vehicula facilisis justo, eu feugiat tortor placerat tincidunt. Aenean vitae est vitae neque venenatis semper. Mauris velit metus, volutpat et fringilla a, auctor eu massa. Suspendisse nec neque eget risus dapibus maximus et id metus. Aliquam vitae eleifend tellus. Etiam vel nisl vitae nisi pulvinar dignissim non in eros. </p>
+				</div>
 
-			<div className='cardGroup team'>
-				<span>Equipe</span>
-				{sellers.map(seller => seller.funcao !== 'Desenvolvedor' && (
-					<div className="seller" key={seller.id}>
-						<img src={seller.imagem} alt={seller.nome} />
+				<div className='cardGroup team'>
+					<span>Equipe</span>
+						{sellers.map(seller => seller.funcao !== 'Desenvolvedor' && (
+							<div className="seller" key={seller.id}>
+								<img src={seller.imagem} alt={seller.nome} />
+								<div className="text">
+									<h1>{seller.nome}</h1>
+									<h2>{seller.funcao}</h2>
+								</div>
+							</div>
+						))}
+				</div>
+
+				<div className='cardGroup site'>
+					<span>Site</span>
+					<div className="seller">
+						<img
+							src={sellers.find(seller => seller.funcao === 'Desenvolvedor').imagem}
+							alt={sellers.find(seller => seller.funcao === 'Desenvolvedor').nome} />
 						<div className="text">
-							<h1>{seller.nome}</h1>
-							<h2>{seller.funcao}</h2>
+							<h1>{sellers.find(seller => seller.funcao === 'Desenvolvedor').nome}</h1>
+							<h2>{sellers.find(seller => seller.funcao === 'Desenvolvedor').funcao}</h2>
+						</div>
+						<div className="links">
+							<a href='https://github.com/iago-mendes' target='_blank' className='github' title='GitHub'>
+								<FiGithub size={30} />
+							</a>
+							<a href='https://www.linkedin.com/in/iago-mendes-21a2361a2/' target='_blank' className='linkedin' title='LinkedIn'>
+								<FiLinkedin size={30} />
+							</a>
 						</div>
 					</div>
-				))}
-			</div>
-
-			<div className='cardGroup site'>
-				<span>Site</span>
-				<div className="seller">
-					<img
-						src={sellers.find(seller => seller.funcao === 'Desenvolvedor').imagem}
-						alt={sellers.find(seller => seller.funcao === 'Desenvolvedor').nome} />
-					<div className="text">
-						<h1>{sellers.find(seller => seller.funcao === 'Desenvolvedor').nome}</h1>
-						<h2>{sellers.find(seller => seller.funcao === 'Desenvolvedor').funcao}</h2>
-					</div>
-					<div className="links">
-						<a href='https://github.com/iago-mendes' target='_blank' className='github' title='GitHub'>
-							<FiGithub size={30} />
-						</a>
-						<a href='https://www.linkedin.com/in/iago-mendes-21a2361a2/' target='_blank' className='linkedin' title='LinkedIn'>
-							<FiLinkedin size={30} />
-						</a>
-					</div>
 				</div>
-			</div>
+			</main>
+
 		</Container>
 	)	
 }
--- src/styles/pages/sobre.ts
+++ src/styles/pages/sobre.ts
@@ -1,158 +1,175 @@
 import styled from 'styled-components'
 
 const Container = styled.div`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	gap: 5rem;
-
-	padding: 5rem;
-	overflow-y: auto;
-
-	.cardGroup
+	main
 	{
-		width: 75%;
-		height: fit-content;
-
-		border: ${p => p.theme.colors.primary} solid 2px;
-		border-radius: 2rem;
-		padding: 3rem;
-
 		display: flex;
 		flex-direction: column;
 		align-items: center;
-		justify-content: center;
+		gap: 5rem;
 
-		position: relative;
+		padding: 5rem;
+		overflow-y: auto;
 
-		span
+		.cardGroup
 		{
-			position: absolute;
+			width: 75%;
+			height: fit-content;
 
-			color: ${p => p.theme.colors.primary};
-			font-family: Ubuntu;
-			font-size: 2.5rem;
-
-			top: -15px;
-			background-color: ${p => p.theme.colors.background};
-
-			padding-left: 1rem;
-			padding-right: 1rem;
-		}
-
-		img
-		{
-			width: 20rem;
-			height: 20rem;
-		}
-
-		.seller
-		{
-			background-color: #fff;
-			padding: 2rem;
-			border-radius: 1rem;
+			border: ${p => p.theme.colors.primary} solid 2px;
+			border-radius: 2rem;
+			padding: 3rem;
 
 			display: flex;
 			flex-direction: column;
 			align-items: center;
-			gap: 1rem;
+			justify-content: center;
+
+			position: relative;
+
+			span
+			{
+				position: absolute;
+
+				color: ${p => p.theme.colors.primary};
+				font-family: Ubuntu;
+				font-size: 2.5rem;
+
+				top: -15px;
+				background-color: ${p => p.theme.colors.background};
+
+				padding-left: 1rem;
+				padding-right: 1rem;
+			}
 
 			img
 			{
-				border-radius: 50rem;
+				width: 20rem;
+				height: 20rem;
 			}
 
-			.text
+			.seller
 			{
+				background-color: #fff;
+				padding: 2rem;
+				border-radius: 1rem;
+
+				display: flex;
+				flex-direction: column;
+				align-items: center;
+				gap: 1rem;
+
+				img
+				{
+					border-radius: 50rem;
+				}
+
+				.text
+				{
+					color: ${p => p.theme.colors.primaryDark};
+					text-align: center;
+
+					h1
+					{
+						font-family: Ubuntu;
+						font-size: 2rem;
+					}
+
+					h2
+					{
+						font-family: Roboto;
+						font-size: 1.5rem;
+					}
+				}
+			}
+		}
+
+		.history
+		{
+			flex-direction: row;
+			gap: 2rem;
+
+			p
+			{
+				font-family: Roboto;
 				color: ${p => p.theme.colors.primaryDark};
-				text-align: center;
-
-				h1
-				{
-					font-family: Ubuntu;
-					font-size: 2rem;
-				}
-
-				h2
-				{
-					font-family: Roboto;
-					font-size: 1.5rem;
-				}
+				font-size: 1.5rem;
 			}
 		}
-	}
-
-	.history
-	{
-		flex-direction: row;
-		gap: 2rem;
-
-		p
+		
+		.team
 		{
-			font-family: Roboto;
-			color: ${p => p.theme.colors.primaryDark};
-			font-size: 1.5rem;
-		}
-	}
-	
-	.team
-	{
-		display: grid;
-		grid-template-columns: repeat(auto-fill, minmax(30rem, 1fr));
-		grid-auto-rows: 25rem;
-		grid-gap: 2rem;
-		align-items: center;
-		justify-items: center;
-	}
-
-	.site
-	{
-		width: fit-content;
-
-		.links
-		{
-			width: 100%;
-
-			display: flex;
+			display: grid;
+			grid-template-columns: repeat(auto-fill, minmax(30rem, 1fr));
+			grid-auto-rows: 30rem;
+			grid-gap: 2rem;
 			align-items: center;
-			justify-content: space-around;
+			justify-items: center;
+		}
 
-			a
+		.site
+		{
+			width: fit-content;
+
+			.links
 			{
-				text-decoration: none;
-				transition: 0.5s;
+				width: 100%;
 
 				display: flex;
 				align-items: center;
-				justify-content: center;
+				justify-content: space-around;
 
-				padding: 1rem;
-				border-radius: 20rem;
-
-				:hover
+				a
 				{
-					color: #fff;
+					text-decoration: none;
+					transition: 0.5s;
+
+					display: flex;
+					align-items: center;
+					justify-content: center;
+
+					padding: 1rem;
+					border-radius: 20rem;
+
+					:hover
+					{
+						color: #fff;
+					}
+				}
+
+				.github
+				{
+					color: #000;
+
+					:hover
+					{
+						background-color: #000;
+					}
+				}
+
+				.linkedin
+				{
+					color: #00f;
+
+					:hover
+					{
+						background-color: #00f;
+					}
 				}
 			}
+		}
+	}
 
-			.github
+	@media(max-width: 850px)
+	{
+		main
+		{
+			padding-left: 2rem;
+			padding-right: 2rem;
+
+			.history
 			{
-				color: #000;
-
-				:hover
-				{
-					background-color: #000;
-				}
-			}
-
-			.linkedin
-			{
-				color: #00f;
-
-				:hover
-				{
-					background-color: #00f;
-				}
+				flex-direction: column;
 			}
 		}
 	}
