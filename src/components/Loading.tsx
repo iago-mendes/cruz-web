5cb05568bf79ddf8af3d993a611593658578f25d
Iago <iagobrazmendes@gmail.com>
2020-12-05 10:56:39 -0300

    create loading spinner

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,4 +1,20 @@
-export default function Loading()
+import Container from '../styles/components/Loading'
+
+const Loading: React.FC = () =>
 {
-	return <h1>Carregando...</h1>
+	return (
+		<Container>
+			<svg>
+				<circle
+					cx='25'
+					cy='25'
+					r='20'
+					fill='none'
+					strokeWidth='4'
+				/>
+			</svg>
+		</Container>
+	)
 }
+
+export default Loading
