3a16fb26303cccd86cb92eaf972d8c46a91234c5
Iago <iagobrazmendes@gmail.com>
2021-02-12 09:40:54 -0300

    quick style fix

--- src/styles/components/Menu.ts
+++ src/styles/components/Menu.ts
@@ -131,30 +131,30 @@ const Container = styled.div<ContainerProps>`
 				gap: 2rem;
 			}
 		}
+	}
 
-		.linkBlock
+	.linkBlock
+	{
+		font-family: Ubuntu;
+		font-weight: 700;
+		font-size: 1.75rem;
+		text-decoration: none;
+
+		color: ${p => p.theme.colors.secondary};
+		border: ${p => p.theme.colors.secondary} 2px solid;
+		border-radius: 100rem;
+
+		padding: 0.75rem;
+		padding-left: 1rem;
+		padding-right: 1rem;
+
+		cursor: pointer;
+		transition: 0.25s;
+
+		:hover
 		{
-			font-family: Ubuntu;
-			font-weight: 700;
-			font-size: 1.75rem;
-			text-decoration: none;
-
-			color: ${p => p.theme.colors.secondary};
-			border: ${p => p.theme.colors.secondary} 2px solid;
-			border-radius: 100rem;
-
-			padding: 0.75rem;
-			padding-left: 1rem;
-			padding-right: 1rem;
-
-			cursor: pointer;
-			transition: 0.25s;
-
-			:hover
-			{
-				background-color: ${p => p.theme.colors.secondary};
-				color: ${p => p.theme.colors.primary};
-			}
+			background-color: ${p => p.theme.colors.secondary};
+			color: ${p => p.theme.colors.primary};
 		}
 	}
 
