3dd4a32cb5fe44e44fe9b202de6441eff39dadae
Iago <iagobrazmendes@gmail.com>
2021-02-15 10:05:50 -0300

    style footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,26 +12,42 @@ const Footer: React.FC = () =>
 				<img src={logo} alt='Cruz representações' className='logo' />
 			</Link>
 
-			<div className='info'>
+			<div className='links'>
 				<div className='group'>
 					<h3>Links do site</h3>
 					<ul>
-						<Link href='/' >Página Principal</Link>
-						<Link href='/contato' >Contato</Link>
-						<Link href='/sobre' >Sobre</Link>
-						<Link href='/sobre/politica-de-privacidade' >Política de Privacidade</Link>
+						<Link href='/' >
+							<a className='link'>
+								Página Principal
+							</a>
+						</Link>
+						<Link href='/contato' >
+							<a className='link'>
+								Contato
+							</a>
+						</Link>
+						<Link href='/sobre' >
+							<a className='link'>
+								Sobre
+							</a>
+						</Link>
+						<Link href='/sobre/politica-de-privacidade' >
+							<a className='link'>
+								Política de Privacidade
+							</a>
+						</Link>
 					</ul>
 				</div>
 				<div className='group'>
 					<h3>Redes sociais</h3>
 					<ul>
-						<a target='_blank' rel='nonreferrer' href='https://www.instagram.com/gilmar.cruzrepresentacoes/' className='social' >
-							<FiInstagram size={25} />
-							<span>@gilmar.cruzrepresentacoes</span>
-						</a>
 						<a target='_blank' rel='nonreferrer' href='https://www.facebook.com/CruzRepresentacoes' className='social' >
 							<FiFacebook size={25} />
 							<span>@CruzRepresentacoes </span>
+						</a>
+						<a target='_blank' rel='nonreferrer' href='https://www.instagram.com/gilmar.cruzrepresentacoes/' className='social' >
+							<FiInstagram size={25} />
+							<span>@gilmar.cruzrepresentacoes</span>
 						</a>
 					</ul>
 				</div>
--- src/styles/components/Footer.ts
+++ src/styles/components/Footer.ts
@@ -1,7 +1,126 @@
 import styled from 'styled-components'
 
 const Container = styled.footer`
+	background-color: ${p => p.theme.colors.primaryDark};
+	padding-top: 3rem;
+	padding-bottom: 3rem;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
 	
+	*
+	{
+		color: ${p => p.theme.colors.secondary};
+		font-family: Ubuntu
+	}
+	
+	.logo
+	{
+		gap: 1rem;
+		width: 25%;
+		cursor: pointer;
+		transition: 0.25s;
+		
+		:hover
+		{
+			transform: scale(1.05);
+		}
+	}
+	
+	.links
+	{
+		width: 60%;
+		display: flex;
+		flex-direction: column;
+		gap: 2rem;
+		
+		.group
+		{
+			display: flex;
+			flex-direction: column;
+			gap: 1rem;
+			width: 100%;
+			
+			h3
+			{
+				font-weight: 700;
+				font-size: 2.5rem;
+				border-left: ${p => p.theme.colors.secondary} 5px solid;
+				padding-left: 1rem;
+			}
+			
+			ul
+			{
+				margin-left: 2rem;
+				display: grid;
+				grid-auto-rows: 3rem;
+				grid-template-columns: repeat(auto-fill, minmax(20rem, 1fr));
+				grid-gap: 1rem;
+				align-items: center;
+			
+				
+				a
+				{
+					text-decoration: none;
+				}
+				
+				.link
+				{
+					font-size: 1.75rem;
+					width: fit-content;
+					
+					::after
+					{
+						content: '';
+						width: 0px;
+						height: 2px;
+						display: block;
+						background: ${p => p.theme.colors.secondary};
+						transition: 0.25s;
+					}
+					
+					:hover::after
+					{
+						width: 100%;
+					}
+				}
+				
+				.social
+				{
+					display: flex;
+					align-items: center;
+					gap: 0.5rem;
+					transition: 0.25s;
+					
+					:hover
+					{
+						text-shadow: 0 0 5px ${p => p.theme.colors.secondary};
+						transform: scale(1.05);
+					}
+					
+					span
+					{
+						font-size: 1.5rem;
+					}
+				}
+			}
+		}
+	}
+	
+	@media(max-width: 700px)
+	{
+		flex-direction: column;
+		
+		.logo
+		{
+			width: 75%;
+		}
+		
+		.links
+		{
+			width: 90%;
+		}
+	}
 `
 
 export default Container
