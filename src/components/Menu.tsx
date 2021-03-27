bfafc1c58f50d2aae98402ece3e04656dab42e32
Iago <iagobrazmendes@gmail.com>
2021-03-27 11:49:31 -0300

    hide companies for unlogged users

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -102,7 +102,7 @@ export default function MenuTabs()
 											overflow: 'hidden'
 										}}
 									>
-										{companies && companies.map((company: Company) => (
+										{(companies && user.id !== 'not-logged') && companies.map((company: Company) => (
 											<Link href={`/catalogo/${company.id}`} key={company.id}>
 												<a className='link'>{company.nome_fantasia}</a>
 											</Link>
