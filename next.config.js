eb8fb0fb326ef75ac3973ddfcbaa4f502d004486
Iago <iagobrazmendes@gmail.com>
2021-06-23 10:47:06 -0300

    setup pwa

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
 const withImages = require('next-images')
-module.exports = withImages({
+const withPWA = require('next-pwa')
+
+module.exports = withPWA(withImages({
 	esModule: true,
 	publicRuntimeConfig:
 	{
 		apiUrl: process.env.API_URL,
 		apiKey: process.env.API_KEY
+	},
+	pwa:
+	{
+		dest: 'public',
+		disable: process.env.NODE_ENV === 'development'
 	}
-})
+}))
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,8 @@
 import Document, {Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps} from 'next/document'
 import {ServerStyleSheet} from 'styled-components'
 
-class MyDocument extends Document {
+class MyDocument extends Document
+{
 	static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps>
 	{
 		const sheet = new ServerStyleSheet()
@@ -28,24 +29,44 @@ class MyDocument extends Document {
 		}
 	}
 			
-  render() {
-    return (
-      <Html lang="pt">
-        <Head>
-            <meta charSet="utf-8" />
-            <link rel="icon" href="/favicon.svg" />
-            <link
-                href="https://fonts.googleapis.com/css2?family=Roboto&family=Ubuntu:wght@400;700&display=swap"
-                rel="stylesheet"
-            />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+	render()
+	{
+		const pwa =
+		{
+			name: 'Cruz',
+			description: 'Excelência em Representação Comercial!',
+		}
+
+		return (
+			<Html lang='pt'>
+				<Head>
+						<meta charSet='utf-8' />
+						<link rel='icon' href='/favicon.svg' />
+						<link
+							href='https://fonts.googleapis.com/css2?family=Roboto&family=Ubuntu:wght@400;700&display=swap'
+							rel='stylesheet'
+						/>
+
+						{/* PWA */}
+						<meta name='application-name' content={pwa.name} />
+						<meta name='apple-mobile-web-app-capable' content='yes' />
+						<meta name='apple-mobile-web-app-status-bar-style' content='default' />
+						<meta name='apple-mobile-web-app-title' content={pwa.name} />
+						<meta name='description' content={pwa.description} />
+						<meta name='format-detection' content='telephone=no' />
+						<meta name='mobile-web-app-capable' content='yes' />
+						<meta name='theme-color' content='#84130B' />
+						<link rel='apple-touch-icon' sizes='192x192' href='/icons/192px.png' />
+						<link rel='manifest' href='/manifest.json' />
+						<link rel='shortcut icon' href='/icons/192px.png' type='image/png' />
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		)
+	}
 }
 
 export default MyDocument
