320f6c0a7228d48f1fd91895a60a424ed3465839
Iago Mendes <iagobrazmendes@gmail.com>
2022-12-27 16:24:18 -0300

    Ignore typescript errors temporarily.

--- next.config.js
+++ next.config.js
@@ -11,5 +11,12 @@ module.exports = withPWA(withImages({
 	pwa: {
 		dest: 'public',
 		disable: process.env.NODE_ENV === 'development'
-	}
+	},
+	typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 }))
